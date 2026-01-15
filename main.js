9a2a4b1154a06bafb0f63a64dde17db3ac2579c9
faizanrasool <faizanrasool6645@gmail.com>
2026-01-15 19:36:03 +0500

    practice on ejs views engine

---
+++ main.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    let siteName = "Netnova"
+    let searchText = "search anything"
+    res.render("index", {siteName:siteName, searchText:searchText})
+})
+
+app.get('/blog/:slug', (req, res) => {
+    let blogTitle = "Netnova  when and why?"
+    let blogContent = "its very good website "
+    res("blogpost", {blogTitle: blogTitle,blogContent: blogContent})
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
